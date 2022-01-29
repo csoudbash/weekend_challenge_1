1bbb278f9fe67500a2b2690b7ee850183ea0631c
Cameron Soudbash <csoudbash@gmail.com>
2022-01-29 16:59:17 -0600

    sorta getting there

--- scripts/client.js
+++ scripts/client.js
@@ -1,43 +1,38 @@
 $('document').ready(onReady);
-let employees =[];//array that the objects of employee will be pushed to
-// let employee = { // object of information regarding the persons information inputed on the DOM
-//     firstName: '',
-//     lastName: '',
-//     idNumber: 0,
-//     jobTitle: '',
-//     annualSalary: 0,
-//     };
-
+let employees =[]; //array that the objects of employee will be pushed to
+let monthlyCosts = 0;
 
 function  onReady(){
     // console.log(employee.annualSalary);
    $('#submitButton').on('click', InputValues);
    $('#deleteButton').on('click', deleteRow);
+
 }
 function InputValues() {//grabbing and emptying values put into inputs on the DOM
     let employee = { // object of information regarding the persons information inputed on the DOM
-        firstName: '',
-        lastName: '',
-        idNumber: 0,
-        jobTitle: '',
-        annualSalary: 0,
+        firstName: $('#first-name').val(),
+        lastName: $('#last-name').val(),
+        idNumber: Number($('#id-number').val()),
+        jobTitle: $('#job-title').val(),
+        annualSalary: Number($('#annual-salary').val()),
         };
+        employees.push(employee);
+         // calculating Monthly costs and update the page with the new value
+         //each time the add button is pressed   
+        let totalSalary=0;
 
-    employee.firstName = $('#first-name').val(); 
-        // console.log(employee.firstName);
-    employee.lastName = $('#last-name').val(); 
-        // console.log(employee.lastName);
-    employee.idNumber = $('#id-number').val();
-        // console.log(employee.idNumber);
-    employee.jobTitle = $('#job-title').val();
-        // console.log(employee.jobTitle);
-    employee.annualSalary = $('#annual-salary').val();
-        // console.log(employee.annualSalary);
-
-        //!!!Not finished !!! Pushing the current created object to the employees array
-    let employeePush = employees.push(employee);
-    console.log(employeePush);
-
+        for (let currentEmployee of employees) {
+        totalSalary += currentEmployee.annualSalary;
+        }
+        
+        monthlyCosts = totalSalary / 12;
+        console.log(monthlyCosts);
+        $('.costsDisplay').empty();
+        $('.costsDisplay').append(`${monthlyCosts}`);
+        if (monthlyCosts > 20000) {
+            $('.h3CostsDisplay').addClass('red');
+        };
+        
         //emptying input fields after the submit button is clicked
         $('#first-name').val('');
         $('#last-name').val(''); 
@@ -45,18 +40,22 @@ function InputValues() {//grabbing and emptying values put into inputs on the DO
         $('#job-title').val('');
         $('#annual-salary').val('');
         console.log(employee);
+        //not working!!! work on later!!
+        //appending to the table
 
-        $('#table').append('<tr id="new-employee.new"></tr>')
-        $('#new-employee').append(`<td>${employee.firstName}</td>`);
-        $('#new-employee').append(`<td>${employee.lastName}</td>`);
-        $('#new-employee').append(`<td>${employee.idNumber}</td>`);
-        $('#new-employee').append(`<td>${employee.jobTitle}</td>`);
-        $('#new-employee').append(`<td>${employee.annualSalary}</td>`);
+        $('#tBody').append('<tr><button>hi</button></tr>');
+        $('#tBody').append(`<tr class='appendNewEmployee'></tr> <button class='deleteButton'>delete</button>
+        <td>$('#first-name').val()</td>
+        <td>$('#last-name').val()</td>
+        <td>$('#id-number').val()</td>
+        <td>$('#job-title').val()</td>
+        <td>$('#annual-salary').val()</td>`);
 
-}
-function appendInputValues() { 
-
-    
+        // $('.appendNewEmployee').append(`<td>${employee.firstName}</td>`);
+        // $('.appendNewEmployee').append(`<td>${employee.lastName}</td>`);
+        // $('.appendNewEmployee').append(`<td>${employee.idNumber}</td>`);
+        // $('.appendNewEmployee').append(`<td>${employee.jobTitle}</td>`);
+        // $('.appendNewEmployee').append(`<td>${employee.annualSalary}</td>`);
 }
 
 function deleteRow() {
